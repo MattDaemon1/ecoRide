a80c0e33ec1cef298216c7c25291d1596d48fcc8
MattDaemon1 <matt@mattkonnect.com>
2024-12-08 16:05:14 +0100

    fin ajout entités

---
+++ src/Entity/Covoiturage.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CovoiturageRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CovoiturageRepository::class)]
+class Covoiturage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateDepart = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $heureDepart = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $lieuDepart = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateArrivee = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $heureArrivee = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $lieuArrivee = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $statut = null;
+
+    #[ORM\Column]
+    private ?int $nbPlace = null;
+
+    #[ORM\Column]
+    private ?float $prixPersonne = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateDepart(): ?\DateTimeInterface
+    {
+        return $this->dateDepart;
+    }
+
+    public function setDateDepart(\DateTimeInterface $dateDepart): static
+    {
+        $this->dateDepart = $dateDepart;
+
+        return $this;
+    }
+
+    public function getHeureDepart(): ?\DateTimeInterface
+    {
+        return $this->heureDepart;
+    }
+
+    public function setHeureDepart(\DateTimeInterface $heureDepart): static
+    {
+        $this->heureDepart = $heureDepart;
+
+        return $this;
+    }
+
+    public function getLieuDepart(): ?string
+    {
+        return $this->lieuDepart;
+    }
+
+    public function setLieuDepart(string $lieuDepart): static
+    {
+        $this->lieuDepart = $lieuDepart;
+
+        return $this;
+    }
+
+    public function getDateArrivee(): ?\DateTimeInterface
+    {
+        return $this->dateArrivee;
+    }
+
+    public function setDateArrivee(\DateTimeInterface $dateArrivee): static
+    {
+        $this->dateArrivee = $dateArrivee;
+
+        return $this;
+    }
+
+    public function getHeureArrivee(): ?\DateTimeInterface
+    {
+        return $this->heureArrivee;
+    }
+
+    public function setHeureArrivee(\DateTimeInterface $heureArrivee): static
+    {
+        $this->heureArrivee = $heureArrivee;
+
+        return $this;
+    }
+
+    public function getLieuArrivee(): ?string
+    {
+        return $this->lieuArrivee;
+    }
+
+    public function setLieuArrivee(string $lieuArrivee): static
+    {
+        $this->lieuArrivee = $lieuArrivee;
+
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    public function getNbPlace(): ?int
+    {
+        return $this->nbPlace;
+    }
+
+    public function setNbPlace(int $nbPlace): static
+    {
+        $this->nbPlace = $nbPlace;
+
+        return $this;
+    }
+
+    public function getPrixPersonne(): ?float
+    {
+        return $this->prixPersonne;
+    }
+
+    public function setPrixPersonne(float $prixPersonne): static
+    {
+        $this->prixPersonne = $prixPersonne;
+
+        return $this;
+    }
+}
